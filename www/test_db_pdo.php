9d47c960c579b6f89d71bb293a1412a420134726
Emilian Nicu <emilian.nicu@gmail.com>
2022-05-05 08:15:10 +0300

    Add missing folders.

---
+++ www/test_db_pdo.php
@@ -0,0 +1,15 @@
+<?php
+
+$DBuser = $_ENV['MYSQL_USER'];
+$DBpass = $_ENV['MYSQL_PASSWORD'];
+$pdo = null;
+
+try{
+    $database = 'mysql:host=mysql_database:3306';
+    $pdo = new PDO($database, $DBuser, $DBpass);
+    echo "Success: A proper connection to MySQL was made! The docker database is great.";    
+} catch(PDOException $e) {
+    echo "Error: Unable to connect to MySQL. Error:\n $e";
+}
+
+$pdo = null;
